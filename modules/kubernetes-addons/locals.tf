1669af1c51c2f4217ba17d0ef915ae1a1fd7418b
Dennis Korbginski <dennis.korbginski@blackbit.de>
2022-06-13 17:25:07 +0200

    Update locals.tf

--- modules/kubernetes-addons/locals.tf
+++ modules/kubernetes-addons/locals.tf
@@ -6,28 +6,30 @@ locals {
 
   # Configuration for managing add-ons via ArgoCD.
   argocd_addon_config = {
-    agones                    = var.enable_agones ? module.agones[0].argocd_gitops_config : null
-    awsEfsCsiDriver           = var.enable_aws_efs_csi_driver ? module.aws_efs_csi_driver[0].argocd_gitops_config : null
-    awsForFluentBit           = var.enable_aws_for_fluentbit ? module.aws_for_fluent_bit[0].argocd_gitops_config : null
-    awsLoadBalancerController = var.enable_aws_load_balancer_controller ? module.aws_load_balancer_controller[0].argocd_gitops_config : null
-    certManager               = var.enable_cert_manager ? module.cert_manager[0].argocd_gitops_config : null
-    clusterAutoscaler         = var.enable_cluster_autoscaler ? module.cluster_autoscaler[0].argocd_gitops_config : null
-    ingressNginx              = var.enable_ingress_nginx ? module.ingress_nginx[0].argocd_gitops_config : null
-    keda                      = var.enable_keda ? module.keda[0].argocd_gitops_config : null
-    metricsServer             = var.enable_metrics_server ? module.metrics_server[0].argocd_gitops_config : null
-    ondat                     = var.enable_ondat ? module.ondat[0].argocd_gitops_config : null
-    prometheus                = var.enable_prometheus ? module.prometheus[0].argocd_gitops_config : null
-    sparkOperator             = var.enable_spark_k8s_operator ? module.spark_k8s_operator[0].argocd_gitops_config : null
-    tetrateIstio              = var.enable_tetrate_istio ? module.tetrate_istio[0].argocd_gitops_config : null
-    traefik                   = var.enable_traefik ? module.traefik[0].argocd_gitops_config : null
-    vault                     = var.enable_vault ? module.vault[0].argocd_gitops_config : null
-    vpa                       = var.enable_vpa ? module.vpa[0].argocd_gitops_config : null
-    yunikorn                  = var.enable_yunikorn ? module.yunikorn[0].argocd_gitops_config : null
-    argoRollouts              = var.enable_argo_rollouts ? module.argo_rollouts[0].argocd_gitops_config : null
-    karpenter                 = var.enable_karpenter ? module.karpenter[0].argocd_gitops_config : null
-    kubernetesDashboard       = var.enable_kubernetes_dashboard ? module.kubernetes_dashboard[0].argocd_gitops_config : null
-    awsCloudWatchMetrics      = var.enable_aws_cloudwatch_metrics ? module.aws_cloudwatch_metrics[0].argocd_gitops_config : null
-    externalDns               = var.enable_external_dns ? module.external_dns[0].argocd_gitops_config : null
+    agones                     = var.enable_agones ? module.agones[0].argocd_gitops_config : null
+    awsEfsCsiDriver            = var.enable_aws_efs_csi_driver ? module.aws_efs_csi_driver[0].argocd_gitops_config : null
+    awsForFluentBit            = var.enable_aws_for_fluentbit ? module.aws_for_fluent_bit[0].argocd_gitops_config : null
+    awsLoadBalancerController  = var.enable_aws_load_balancer_controller ? module.aws_load_balancer_controller[0].argocd_gitops_config : null
+    certManager                = var.enable_cert_manager ? module.cert_manager[0].argocd_gitops_config : null
+    clusterAutoscaler          = var.enable_cluster_autoscaler ? module.cluster_autoscaler[0].argocd_gitops_config : null
+    ingressNginx               = var.enable_ingress_nginx ? module.ingress_nginx[0].argocd_gitops_config : null
+    keda                       = var.enable_keda ? module.keda[0].argocd_gitops_config : null
+    metricsServer              = var.enable_metrics_server ? module.metrics_server[0].argocd_gitops_config : null
+    ondat                      = var.enable_ondat ? module.ondat[0].argocd_gitops_config : null
+    prometheus                 = var.enable_prometheus ? module.prometheus[0].argocd_gitops_config : null
+    sparkOperator              = var.enable_spark_k8s_operator ? module.spark_k8s_operator[0].argocd_gitops_config : null
+    tetrateIstio               = var.enable_tetrate_istio ? module.tetrate_istio[0].argocd_gitops_config : null
+    traefik                    = var.enable_traefik ? module.traefik[0].argocd_gitops_config : null
+    vault                      = var.enable_vault ? module.vault[0].argocd_gitops_config : null
+    vpa                        = var.enable_vpa ? module.vpa[0].argocd_gitops_config : null
+    yunikorn                   = var.enable_yunikorn ? module.yunikorn[0].argocd_gitops_config : null
+    argoRollouts               = var.enable_argo_rollouts ? module.argo_rollouts[0].argocd_gitops_config : null
+    karpenter                  = var.enable_karpenter ? module.karpenter[0].argocd_gitops_config : null
+    kubernetesDashboard        = var.enable_kubernetes_dashboard ? module.kubernetes_dashboard[0].argocd_gitops_config : null
+    awsCloudWatchMetrics       = var.enable_aws_cloudwatch_metrics ? module.aws_cloudwatch_metrics[0].argocd_gitops_config : null
+    externalDns                = var.enable_external_dns ? module.external_dns[0].argocd_gitops_config : null
+    secretsStoreCsiDriver      = var.enable_secrets_store_csi_driver ? module.secrets_store_csi_driver[0].argocd_gitops_config : null
+    csiSecretsStoreProviderAws = var.enable_secrets_store_csi_driver_provider_aws ? module.csi_secrets_store_provider_aws[0].argocd_gitops_config : null
   }
 
   addon_context = {
