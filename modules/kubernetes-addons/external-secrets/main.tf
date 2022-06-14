e3ac588d5c2507a85afdce51178e7f1bc4515cdb
Dennis Korbginski <dennis.korbginski@blackbit.de>
2022-06-14 19:14:40 +0200

    Pass IRSA and helm config

--- modules/kubernetes-addons/external-secrets/main.tf
+++ modules/kubernetes-addons/external-secrets/main.tf
@@ -11,7 +11,9 @@ resource "kubernetes_namespace_v1" "external_secrets" {
 module "helm_addon" {
   source            = "../helm-addon"
   manage_via_gitops = var.manage_via_gitops
+  set_values        = local.set_values
   helm_config       = local.helm_config
+  irsa_config       = local.irsa_config
   addon_context     = var.addon_context
 
   depends_on = [kubernetes_namespace_v1.external_secrets]
